eb85778c100b16410b59e89b5619e88daa89db0f
Rohith Preetham Sunkara <sunkararohith008@gmail.com>
2021-05-26 00:03:18 -0400

    Added users folder to manage users for the admin

---
+++ admin/users/edit.php
@@ -0,0 +1,120 @@
+<?php include("../../path.php"); ?>
+<?php include(ROOT_PATH . "/app/controllers/users.php"); 
+adminOnly();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="description" content="This blog website educate and guide people to build their own machines that they keep generating money to wealth.">
+        <meta name="robots" content="index, follow">
+        <meta name="author" content="Rohith Preetham Sunkara, Arshveer Uppal">
+
+        <!-- Icon - Faviicon -->
+        <link rel="icon" type="image/png" href="../../logo-coin.png" />
+        <!-- Font Awesome -->
+        <link rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+            integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+            crossorigin="anonymous">
+
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Candal|Lora"
+            rel="stylesheet">
+
+        <!-- Custom Styling -->
+        <link rel="stylesheet" href="../../assets/css/style.css">
+
+        <!-- Admin Styling -->
+        <link rel="stylesheet" href="../../assets/css/admin.css">
+
+        <title>Admin Section - Edit User</title>
+    </head>
+
+    <body>
+        
+    <?php include(ROOT_PATH . "/app/includes/adminHeader.php"); ?>
+
+        <!-- Admin Page Wrapper -->
+        <div class="admin-wrapper">
+
+        <?php include(ROOT_PATH . "/app/includes/adminSidebar.php"); ?>
+
+
+            <!-- Admin Content -->
+            <div class="admin-content">
+                <div class="button-group">
+                    <a href="create.php" class="btn btn-admin">Add User</a>
+                    <a href="index.php" class="btn btn-admin">Manage Users</a>
+                </div>
+
+
+                <div class="content">
+
+                    <h2 class="page-title">Edit User</h2>
+
+                    <?php include(ROOT_PATH . "/app/helpers/formErrors.php"); ?>
+
+                    <form action="edit.php" method="post">
+                        <input type="hidden" name="id" value="<?php echo $id; ?>" >
+                        <div>
+                            <label>Username</label>
+                            <input type="text" name="username" value="<?php echo $username; ?>" class="text-input">
+                        </div>
+                        <div>
+                            <label>Email</label>
+                            <input type="email" name="email" value="<?php echo $email; ?>" class="text-input">
+                        </div>
+                        <div>
+                            <label>Password</label>
+                            <input type="password" name="password" value="<?php echo $password; ?>" class="text-input">
+                        </div>
+                        <div>
+                            <label>Password Confirmation</label>
+                            <input type="password" name="passwordConf" value="<?php echo $passwordConf; ?>" class="text-input">
+                        </div>
+                        <div>
+                            <?php if (isset($admin) && $admin == 1): ?>
+                                <label>
+                                    <input type="checkbox" name="admin" checked>
+                                    Admin
+                                </label>
+                            <?php else: ?>
+                                <label>
+                                    <input type="checkbox" name="admin">
+                                    Admin
+                                </label>
+                            <?php endif; ?>
+                            
+                        </div>
+
+                        <div>
+                            <button type="submit" name="update-user" class="btn btn-big">Update User</button>
+                        </div>
+                    </form>
+
+                </div>
+
+            </div>
+            <!-- // Admin Content -->
+
+        </div>
+        <!-- // Page Wrapper -->
+
+
+
+        <!-- JQuery -->
+        <script
+            src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <!-- Ckeditor -->
+        <script
+            src="https://cdn.ckeditor.com/ckeditor5/12.2.0/classic/ckeditor.js"></script>
+        <!-- Custom Script -->
+        <script src="../../assets/js/scripts.js"></script>
+
+    </body>
+
+</html>

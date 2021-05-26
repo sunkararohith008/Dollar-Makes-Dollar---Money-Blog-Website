875b9b827b43276ed5f41599829e929e83bb180a
Arshveer Uppal <64243859+Arshveer0708@users.noreply.github.com>
2021-05-25 23:13:23 -0400

    Added about.php

---
+++ about.php
@@ -0,0 +1,63 @@
+<?php include("path.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="description" content="This blog website educate and guide people to build their own machines that they keep generating money to wealth.">
+  <meta name="robots" content="index, follow">
+  <meta name="author" content="Rohith Preetham Sunkara, Arshveer Uppal">
+
+  <!-- Icon - Faviicon -->
+  <link rel="icon" type="image/png" href="logo-coin.png" />
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Candal|Lora" rel="stylesheet">
+
+  <!-- Custom Styling -->
+  <link rel="stylesheet" href="assets/css/style.css">
+
+  <title>About </title>
+</head>
+
+<body>
+ 
+
+  <?php include(ROOT_PATH . "/app/includes/header.php"); ?>
+
+  <h5>Your privacy is important to us.</h5>
+        
+        <p>The information and services published on this website may include inaccuracies or errors, including, without limitation, pricing, photographs, product descriptions, etc. In particular, Dream Destination does not guarantee the accuracy of, and disclaims all liability for any errors or other inaccuracies relating to the information and description of content displayed on the site </p>
+
+
+        <h4>Links to Other Sites</h4>
+        <p>We also involve other parties to provide you with the services. This includes, for example, viator, skyscanner advertisers, rideshare facebook and whatsapp groups of Canada, taxi companies, car-rental companies,etc.</p>
+        <p>The information posted on the site includes links to information created and maintained by stakeholders and other public and/or private organizations. These links are provided solely for the user’s information and convenience.There are several links through the Dream Destination website that transfer you to other websites that are not under this Policy. When you click on such links to access these sites, the current Policy no longer applies. We are just the third-party website. Our aim is to provide you a single platform for everything needed for trip, whether it is booking hotel, providing readymade tour packages, booking air-tickets, providing tickets to various trip activities or information regarding mode of transport, etc. We are not responsible for the accuracy of the information. </p>
+
+        <p>If you register to use the site: we may ask you for certain contact details. All contact information is stored on our server, which has built-in security and is not available to unauthorized persons.</p>
+
+        <h6>What do we use your information for? </h6>
+
+        <h6>Any of the information we collect from you may be used in one of the following ways: </h6>
+
+
+
+  <?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
+
+
+  <!-- JQuery -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+
+  <!-- Custom Script -->
+  <script src="assets/js/scripts.js"></script>
+
+</body>
+
+</html>
